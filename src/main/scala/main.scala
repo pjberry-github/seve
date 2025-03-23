07eda5255821ab37c974db5fa90be91cf817bca9
pjberry <pjberry@wellprincipled.com>
2025-03-22 18:17:44 -0600

    reorganize, show summoning

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -3,6 +3,26 @@ import scala.deriving.Mirror.ProductOf
 
 @main
 def main(): Unit =
+  demoManualMirror()
+
+  println()
+
+  demoSummonMirror()
+
+
+def demoSummonMirror(): Unit =
+  val caseClassProduct = CaseClassProduct("a", 1)
+
+  val mirror: ProductOf[CaseClassProduct] = summon[Mirror.ProductOf[CaseClassProduct]]
+
+  val mirrorProductCompilerTuple = Tuple.fromProductTyped(caseClassProduct)(using mirror)
+  val mirrorProductCompilerCaseClass = mirror.fromProduct(mirrorProductCompilerTuple) // remember, Tuple and `case class` are both products
+
+  println("***  Summon  ***")
+  println(mirrorProductCompilerTuple)     //(a,1)
+  println(mirrorProductCompilerCaseClass) //CaseClassProduct(a,1)
+
+def demoManualMirror(): Unit =
   val caseClassProduct = CaseClassProduct("a", 1)
 
   val mirrorProductManual: ProductOf[CaseClassProduct]= new Mirror.Product {
@@ -15,12 +35,13 @@ def main(): Unit =
     }
   }
 
-  val x = Tuple.fromProductTyped(caseClassProduct)(using mirrorProductManual)
-  println(x) //(a,1)
+  val mirrorProductManualTuple = Tuple.fromProductTyped(caseClassProduct)(using mirrorProductManual)
+  val mirrorProductManualCaseClass = mirrorProductManual.fromProduct(mirrorProductManualTuple) // remember, Tuple and `case class` are both products
 
-  // remember, Tuple and `case class` are both products
-  val y = mirrorProductManual.fromProduct(x)
-  print(y) //CaseClassProduct(a,1)
+  println("***  Manual  ***")
+  println(mirrorProductManualTuple)     //(a,1)
+  println(mirrorProductManualCaseClass) //CaseClassProduct(a,1)
+
 
 
 case class CaseClassProduct(parameterOne: String, parameterTwo: Int)
