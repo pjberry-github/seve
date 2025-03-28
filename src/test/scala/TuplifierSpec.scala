5c404191bcd929c5b84dc7f29694ad6ac6276b10
pjberry <pjberry@wellprincipled.com>
2025-03-28 12:33:46 -0600

    safety commit.

--- src/test/scala/TuplifierSpec.scala
+++ src/test/scala/TuplifierSpec.scala
@@ -1,4 +1,6 @@
-
+import scala.:+
+import scala.compiletime.{erasedValue, summonInline}
+import scala.deriving.Mirror
 
 
 class TuplifierSpec extends munit.FunSuite {
@@ -16,8 +18,34 @@ class TuplifierSpec extends munit.FunSuite {
       string = "abc"
     )
 
-    val obtained = Tuplifier.tuplify(caseClassOfPrimitives)
+    val initialTuple = Tuple.fromProductTyped(caseClassOfPrimitives)
+    val obtained = Tuplifier.deconstruct(Tuple(), initialTuple)
+
     val expected = (true, 'a', -1: Byte, -2: Short, 1000: Int, 10_000_000_000L, 1.2F, 2.3: Double, "abc")
+
+    assertEquals(obtained, expected)
+  }
+
+  test("case class with a case class") {
+    case class CaseClassOfCaseClass(caseClassOfPrimitives: CaseClassOfPrimitives)
+
+    val caseClassOfPrimitives = CaseClassOfPrimitives(
+      boolean = true,
+      char = 'a',
+      byte = -1,
+      short = -2,
+      int = 1000,
+      long = 10_000_000_000L,
+      float = 1.2F,
+      double = 2.3,
+      string = "abc"
+    )
+
+    val caseClassOfCaseClass = CaseClassOfCaseClass(caseClassOfPrimitives)
+
+    val initialTuple = Tuple.fromProductTyped(caseClassOfCaseClass)
+    val obtained = Tuplifier.deconstruct(Tuple(), initialTuple)
+    val expected = Tuple((true, 'a', -1: Byte, -2: Short, 1000: Int, 10_000_000_000L, 1.2F, 2.3: Double, "abc"))
 
     assertEquals(obtained, expected)
   }
@@ -39,12 +67,33 @@ case class CaseClassOfPrimitives(
 
 object Tuplifier {
 
-  type Elem[X] = X match
-    case String => Char
+//  def tuplify[P <: Product](p: P): Tuple = {
+//    Tuple.fromProduct(p)
+//  }
+//
+//  private def deconstructProduct[P <: Product](product: P)(using Mirror.ProductOf[P]): Tuple = {
+//    val x = Tuple.fromProductTyped(product)
+//
+//  }
 
+//  Tuple.fromProductTyped(product) match {
+//private def deconstruct[P <: Product](resultTuple: Tuple, parts: Tuple)(using mirror: Mirror.ProductOf[P]): Tuple = {
+  def deconstruct(resultTuple: Tuple, parts: Tuple): Tuple = {
+    parts match
+      case EmptyTuple => resultTuple
+      case head *: tail =>
+        
 
+        head match
+          case p: Product =>
+            val asTuple = deconstruct(Tuple(), Tuple.fromProduct(p))
+            println(asTuple)
+            deconstruct(resultTuple, asTuple *: tail)
+          case _ =>
+            deconstruct(resultTuple :* head, tail)
 
-  def tuplify[P <: Product](p: P): Tuple = {
-    Tuple.fromProduct(p)
   }
 }
+
+
+  
